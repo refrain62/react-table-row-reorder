de6713ff7082347d02abf119f7a1b5f027fb2f9f
refrain62 <nao_h13@hotmail.com>
2024-11-21 12:37:56 +0900

    取得したデータをテーブルで表示

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+
 const UserTable = () => {
-  return <div style={{ margin: '2em' }}>UserTable</div>
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const users = await response.json();
+      setUsers(users);
+    };
+
+    getUsers(users);
+  }, []);
+
+  return (
+    <div style={{ margin: '2em' }}>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>名前</th>
+            <th>ユーザ名</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default UserTable;
